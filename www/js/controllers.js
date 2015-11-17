d5034d79c8d36cb4026fa9790bbfc5c989a0cebc
Trev <trevor.pawlewicz@gmail.com>
2015-11-16 19:32:37 -0800

    some comments

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
 var app = angular.module('caffeinehit.controllers', []);
 
+//                 injecting dependencies ($scope is an Angular service)
 app.controller("YelpController", function ($scope, YelpService) {
 	$scope.yelp = YelpService;
 });
